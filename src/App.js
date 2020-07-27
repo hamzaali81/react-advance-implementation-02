334d63b3c871cbb20516ab950fc3ce9de7bd9007
hamzaali <hamza54834@gmail.com>
2020-07-27 04:03:42 -0700

    state advance concept recovery point

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import React, { Component } from 'react';
 //function two purpose 
 //1.JSX render
 //2.Event apply in function fire
-//Component mein jitna bhi functions bana ga wo direct access nhi hongy this ka sath acccess honga
+//Component mein jitna bhi functions bana ga wo direct this keyword ka sath access nhi hongy this ka sath acccess honga
+//JIS function meinthis mil jay wo binding khelata hai
+
+
 
 class App extends Component {
   constructor(){
@@ -17,10 +20,23 @@ class App extends Component {
       // family:{
       //   brother: 3,
         
+
       // }
+      // text: 'Zebra'
     }
+  // this.stateUpdate=this.stateUpdate.bind(this);
+  // this.stateUpdate=this.stateUpdate.bind(this);
   }
 
+
+
+
+stateUpdate(el){
+  console.log("hello",this);
+  // this.setState({
+    
+  // })
+}
 // updateState(){
 //   if(this.state.condition){
 
@@ -42,45 +58,66 @@ class App extends Component {
 //   )
 // }
 
+//function render ka honga wo ek sath or baki render ka uper
+
+//{{}} {}expression {}object
 
 //super call component constructor
-    updateState(){
-      console.log("one");
-      if(this.state.condition){
-        console.log("two");
-        this.setState({
-       text:'Sylani Pakistan',
-      //  condition: false
-condition: !this.state.condition,
-family:{
-  brother: 3,
+//     updateState(){
+//       console.log("one");
+//       if(this.state.condition){
+//         console.log("two");
+//         this.setState({
+//        text:'Sylani Pakistan',
+//       //  condition: false
+// condition: !this.state.condition,
+// family:{
+//   brother: 3,
   
-}
-    })
-      }
-      else{
-        console.log("hello");
-        this.setState({
-        text: "Sylani meri jaan",
-          condition: true
-        })
-      }
-    }
+// }
+//     })
+//       }
+//       else{
+//         console.log("hello");
+//         this.setState({
+//         text: "Sylani meri jaan",
+//           condition: true
+//         })
+//       }
+//     }
+
+// mainheader(){
+//     return(
+//     <h1>Hello World Pakistan</h1>
+//     )
+  
+//   }
+
 
 
   render() {
     return (
       <div>
+{/* <h1>{this.state.text}</h1> */}
+{/* <button onClick={()=>{this.stateUpdate()}}>Click Me</button> */}
+{/* onclick this not component this */}
+
+{/* function mein onclick ka mila ga ar () mein component ka mila ga return*/}
+{/* <button onClick={this.stateUpdate}>Click Me</button> */}
+<button onClick={this.stateUpdate.bind(this,"hello")}>Click Me</button>
+
       {/* <h1>{this.state.text}</h1>
         <button onClick={()=>{this.updateState()}}>Hello Btn</button>
         <p>{this.checkJSX()}</p> */}
 
-        <h1>{this.state.text}</h1>
-        <button onClick={()=>{this.updateState()}}>Hello Btn</button>
+        {/* <h1>{this.state.text}</h1> */}
+        {/* <button onClick={()=>{this.updateState()}}>Hello Btn</button>
         <p>{this.state.family && this.state.family.brother }</p>
+   <h2>{this.mainheader()}</h2> */}
       </div>
     );
   }
 }
+
 
 export default App;
